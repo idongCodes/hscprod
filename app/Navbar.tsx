431a00bebada2b0b3209fdc3665aeababd30d501
idongCodes <i.d.essien@gmail.com>
2026-01-17 10:37:26 -0500

    Add Venue Page with Mock Data and Nav Icon

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -49,10 +49,11 @@ export default function Navbar() {
     { 
       name: "IRL", 
       href: "/irl", 
-      // Venue/Location Icon
+      // Location Pin Icon
       icon: (
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657l-1.414-1.414a2 2 0 00-2.828 0l-4.586-4.586a2 2 0 00-2.828 0L16 16m-2-2l1.414-1.414a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 10a3 3 0 106 0 3 3 0 00-6 0z" />
         </svg>
       )
     },
